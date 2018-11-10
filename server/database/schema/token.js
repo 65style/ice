8de75b1552654b8e2fc4356ce598cf33f1c90944
65style <lym210119@gmail.com>
2018-11-11 05:00:35 +0800

    token save

---
+++ server/database/schema/token.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TokenSchema = new mongoose.Schema({
+  name: String,
+  token: String,
+  expires_in: String,
+  meta: {
+    createdAt: {
+      type: Date,
+      default: new Date
+    },
+    updatedAt: {
+      type: Date,
+      default: new Date
+    }
+  }
+})
+
+TokenSchema.pre('save', function(next) {  // 在每一条数据保存之前
+  if (this.isNew) { // 如果是新增的, 就把创建时间和更新时间重置为当前时间
+    this.meta.createdAt = this.meta.updatedAt = Date.now()
+  } else {  // 不是新增的则只重制更新时间
+    this.meta.updateAt = Date.now()
+  }
+  next()
+})
+
+TokenSchema.static = {  // 添加静态方法 
+  async getAccessToken () {
+    const token = await this.findOne({
+      name: 'access_token'
+    }).exec()
+
+    return token
+  },
+
+  async setAccessToken (data) {
+    let token = await this.findOne({
+      name: 'access_token'
+    }).exec()
+
+    if (token) {
+      token.token = data.access_token
+      token.expires_in = data.expires_in
+    } else {
+      token = new Token({
+        name: 'access_token',
+        token: data.access.token,
+        expires_in: data.expires_in
+      })
+    }
+
+    await token.save()
+
+    return data
+  }
+}
+
+const Token = mongoose.model('Token', TokenSchema)
--- server/index.js
+++ server/index.js
@@ -1,47 +1,61 @@
 import Koa from 'koa'
 import { Nuxt, Builder } from 'nuxt'
+import R from 'ramda'
+import { resolve } from 'path'
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env === 'production')
+
+const r = path => resolve(__dirname, path)
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+const MIDDLEWARES = ['database', 'router']
 
 class Server {
   constructor () {
     this.app = new Koa()
-    // this.useMiddleWare(this.app) // 中间件
+    this.useMiddleWares(this.app)(MIDDLEWARES) // 中间件
   }
-
+  // R.map 数组的每个成员依次执行某个函数
+  // R.compose 将多个函数合并成一个函数，从右到左执行, 右侧函数的输出作为左侧函数的输入
+  useMiddleWares (app) {
+    return R.map(R.compose(
+      R.map(i => i(app)),               // 传入 app
+      require,                          // 引入模块
+      i => `${r('./middlewares')}/${i}` // 拿到路径 ./middlewares/router
+    ))
+  }
   
-}
-async function start () {
-  const app = new Koa()
-  const host = process.env.HOST || '127.0.0.1'
-  const port = process.env.PORT || 3000
-
-  // Import and Set Nuxt.js options
-  let config = require('../nuxt.config.js')
-  config.dev = !(app.env === 'production')
-
-  // Instantiate nuxt.js
-  const nuxt = new Nuxt(config)
-
-  // Build in development
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  }
-
-  app.use(async (ctx, next) => {
-    await next()
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
+  async start () {
+  
+    // Instantiate nuxt.js
+    const nuxt = new Nuxt(config)
+  
+    // Build in development
+    if (config.dev) {
+      const builder = new Builder(nuxt)
+      await builder.build()
+    }
+  
+    this.app.use(async (ctx, next) => {
+      await next()
+      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+      return new Promise((resolve, reject) => {
+        ctx.res.on('close', resolve)
+        ctx.res.on('finish', resolve)
+        nuxt.render(ctx.req, ctx.res, promise => {
+          // nuxt.render passes a rejected promise into callback on error.
+          promise.then(resolve).catch(reject)
+        })
       })
     })
-  })
-
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+  
+    this.app.listen(port, host)
+    console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+  }
 }
 
-start()
+const app = new Server()
+
+app.start()
