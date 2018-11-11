9d914672e2c7d5c35f4d54316daee16de02f7a5b
65style <lym210119@gmail.com>
2018-11-11 23:40:51 +0800

    微信消息解析与回复模板封装

--- server/wechat-lib/middleware.js
+++ server/wechat-lib/middleware.js
@@ -33,33 +33,20 @@ export default function (opts, reply) {
           limit: '1mb',
           encoding: ctx.charset
         })
-        console.log('data: ' + data)
         const content = await util.parseXMl(data) // 解析XML
-        // const message = util.formatMessage(content.xml) 
+        const message = util.formatMessage(content.xml) 
 
-        console.log(content)
-        ctx.weixin = {}
+        ctx.weixin = message
 
         await reply.apply(ctx, [ctx, next])
 
         const replyBody = ctx.body
         const msg = ctx.weixin
-        // const xml = util.tpl(replyBody, msg)
-
-        console.log(replyBody)
-
-        const xml = `<xml>
-          <ToUserName><![CDATA[${content.xml.FromUserName[0]}]]></ToUserName>
-          <FromUserName><![CDATA[${content.xml.ToUserName[0]}]]></FromUserName>
-          <CreateTime>12345678</CreateTime>
-          <MsgType><![CDATA[text]]></MsgType>
-          <Content><![CDATA[${replyBody}]]></Content>
-          </xml>`
+        const xml = util.tpl(replyBody, msg)
 
         ctx.status = 200
         ctx.type = 'application/xml'
         ctx.body = xml
-        console.log(ctx.body)
       }
   }
 }
--- server/wechat/reply.js
+++ server/wechat/reply.js
@@ -3,7 +3,6 @@ const tip = '我的卡丽熙， 欢迎来到河间地\n' +
 
 export default async (ctx, netx) => {
   const message = ctx.weixin
-  console.log('smddddddddddddd')
   console.log(message)
   ctx.body = tip
 }
